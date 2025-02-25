a2a28aa8aa307474e4b2e3d5b4e1fc36142857de
Mashafi-05 <mashafi20042@gmail.com>
2025-02-25 22:20:31 +0600

    first commit

---
+++ AgeInDays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+
+    int N;
+    scanf("%d", &N);
+    int years = N / 365;
+    int months = (N % 365) / 30;
+    int days = (N % 365) % 30;
+    printf("%d years \n%d months \n%d days", years, months, days);
+
+    return 0;
+}
